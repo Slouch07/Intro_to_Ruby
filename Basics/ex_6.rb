0d6a0b9c9f9f728e977dc0f9f62cbcad5add6254
Ryan Bowler <rbowler0707@gmail.com>
2020-08-31 21:56:14 -0700

    added exercises 6 and 7

---
+++ Basics/ex_6.rb
@@ -0,0 +1,7 @@
+float_1 = 22.4 ** 2
+float_2 = 14.7 ** 2
+float_3 = 10.6 ** 2
+
+puts float_1
+puts float_2
+puts float_3
---
+++ Basics/ex_7.rb
@@ -0,0 +1,2 @@
+# This error tells me that on line 2 of my Ruby code, Ruby is expecting a right curly
+# brace and not a right parenthesis.
